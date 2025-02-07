012e876384790bee01e7c42cdd62d7ec73fb5c3a
kaylamcculloch <kayla@kaylamclochsmbp.mynetworksettings.com>
2025-02-06 22:30:38 -0500

    kam

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 const osakaBtn = document.getElementById('osakaBtn');
 const osakaPopUp = document.getElementById('osakaPopUp');
 
+const kamBtn = document.getElementById('kamBtn');
+const kamPopUp = document.getElementById('kamPopUp');
+
+
 const popupBtn = document.getElementById('popupBtn');
 const popup = document.getElementById('popup');
 //const closeBtn = document.getElementById('closeBtn');
@@ -14,6 +18,12 @@ osakaBtn.onclick = function() {
     osakaPopUp.style.display = 'flex';
 }
 
+// Show kamamura
+kamBtn.onclick = function() {
+    // Show the popup
+    kamPopUp.style.display = 'flex';
+}
+
 // Show the popup when the button is clicked
 popupBtn.onclick = function() {
     // Trigger confetti effect
@@ -23,19 +33,6 @@ popupBtn.onclick = function() {
     popup.style.display = 'flex';
 }
 
-/*
-// Close the popup when the close button is clicked
-closeBtn.onclick = function() {
-    popup.style.display = 'none';
-}
-
-// Close the popup if the user clicks outside the popup content
-window.onclick = function(event) {
-    if (event.target === popup) {
-        popup.style.display = 'none';
-    }
-}
-*/
 
 // Attach event listener to each close button
 closeBtns.forEach(function(btn) {
@@ -43,6 +40,8 @@ closeBtns.forEach(function(btn) {
         // Close the respective modal when clicking on a close button
         popup.style.display = "none";
         osakaPopUp.style.display = "none";
+        kamPopUp.style.display = "none";
+
     }
 });
 
